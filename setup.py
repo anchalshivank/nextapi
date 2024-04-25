963ea6b55653730263c2cf3c36e0f58438adc632
anchalshivank <anchalshivank99@gmail.com>
2024-04-25 14:25:07 +0530

    added alembic feature to microservices

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="next-api-starter",  # Replace with your desired package name
-    version="0.1.3",  # Start with version 0.1.0 for initial release
+    version="0.1.4",  # Start with version 0.1.0 for initial release
     description="Starter template for Next.js and FastAPI projects",
     long_description="...",  # Add detailed description in README format
     long_description_content_type="text/markdown",
